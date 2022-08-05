03b6d119d15c756573e070e51bc8d2b344418db2
patcard21 <71289158+patcard21@users.noreply.github.com>
2022-08-05 18:20:28 -0400

    Imported some components to index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import WeatherSearch from "./WeatherSearch";
-
+import WeatherSearch from "./WeatherSearch.js";
+import Search from "./search.js";
 import "./sky.jpg";
 
 import "./styles.css";
 
 function App() {
-	return (
-		<div className="App" style={{ backgroundImage: "./sky.jpg" }}>
-			<h1> Weather App</h1>
-			<WeatherSearch />
-		</div>
-	);
+  return (
+    <div className="App" style={{ backgroundImage: "./sky.jpg" }}>
+      <h1> Weather App</h1>
+      <WeatherSearch />
+    </div>
+  );
 }
 
 const container = document.getElementById("root");
